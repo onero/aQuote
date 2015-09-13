c1b985a000a85a0c899d8cc3097d4c374562eab1
adamino <adamlars90@gmail.com>
2015-09-13 17:48:03 +0200

    Initial commit

---
+++ app/src/main/java/com/adamino/aquote/QuoteActivity.java
@@ -0,0 +1,54 @@
+package com.adamino.aquote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class QuoteActivity extends AppCompatActivity {
+
+    private QuoteBook mQuoteBook = new QuoteBook();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote);
+
+        // Declare View variables
+        final TextView textQuote = (TextView) findViewById(R.id.textQuote);
+        final TextView textAuthor = (TextView) findViewById(R.id.textAuthor);
+        final ImageView imageAuthor = (ImageView) findViewById(R.id.imageAuthor);
+        Button quoteButton = (Button) findViewById(R.id.quoteButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(mQuoteBook.mQuotes.length);
+
+                String Quote = mQuoteBook.mQuotes[randomNumber][0];
+                String Author = mQuoteBook.mQuotes[randomNumber][1];
+                // Update the Quote
+                textQuote.setText(Quote);
+                textAuthor.setText(Author);
+
+
+                if (randomNumber == 0) {
+                    imageAuthor.setImageResource(R.drawable.borge);
+                } else if (randomNumber == 1) {
+                    imageAuthor.setImageResource(R.drawable.ward);
+                } else if (randomNumber == 2) {
+                    imageAuthor.setImageResource(R.drawable.einstein);
+                }
+
+            }
+        };
+
+        quoteButton.setOnClickListener(listener);
+    }
+}
---
+++ app/src/main/java/com/adamino/aquote/QuoteBook.java
@@ -0,0 +1,17 @@
+package com.adamino.aquote;
+
+/**
+ * Created by Adam on 13-09-2015.
+ */
+public class QuoteBook {
+
+    public String[][] mQuotes = {
+            {"A smile is the shortest distance between two people",
+                    "Victor Borge"},
+            {"If you can imagine it, you can achieve it, if you can dream it, you can become it",
+                    "William Arthur Ward"},
+            {"The true sign of intelligence is not knowledge, but imagination.",
+                    "Albert Einstein"}
+    };
+
+}
